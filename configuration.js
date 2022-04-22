1e081fc32f412572272307ac74edfe79f650b2b0
Ajjah <hamzaajja123@gmail.com>
2022-04-22 19:40:25 +0200

    front added

---
+++ configuration.js
@@ -0,0 +1,2 @@
+export const marketplaceAddress = "0x7bE5794eD02D8Cc86eAa2Da86764b460DA144Dbb"
+export const link_id = "https://rinkeby.infura.io/v3/21cbaf8465024bacb08b41095418ed1e"
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,40 @@
 import '../styles/globals.css'
 import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps })=>{
+  return (
+    <div>
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-6 text-pink-500">
+              Sell NFT
+            </a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6 text-pink-500">
+              My NFTs
+            </a>
+          </Link>
+          <Link href="/dashboard">
+            <a className="mr-6 text-pink-500">
+              Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+export default MyApp ;
+
+
+
