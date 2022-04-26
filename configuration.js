32423bd31b339472b35c465d0fade2813e652269
Ajjah <hamzaajja123@gmail.com>
2022-04-26 16:26:13 +0200

    .env adeed

--- configuration.js
+++ configuration.js
@@ -1,3 +1,3 @@
-export const marketplaceAddress = "0xad1a7823C797cc64DD8Db1cd7d7837c66896b61C";
+export const marketplaceAddress = "0xad1a7823C797cc64DD8Db1cd7d7837c66896b61C"
 export const link_id = "https://rinkeby.infura.io/v3/21cbaf8465024bacb08b41095418ed1e";
-export const Private_Key = "a6b44160d8b3ceedf2541cfa9fb748f4523e1e3801fd71e19f4760e3081b374a";
+export const Private_Key = process.env.PRIVATE_KEY
